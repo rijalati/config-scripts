b8346189b953e135f306123e362693f38e92bae6
Ritchie J. Latimore <rijalati@gmail.com>
2015-01-03 04:50:36 +0000

    make pacman cmds succeed

--- arch-config.sh
+++ arch-config.sh
@@ -5,8 +5,8 @@
 
 cd /tmp
 sudo pacman -Syy
-sudo pacman -Syu --noconfirm
-sudo pacman -S --needed --noconfirm base-devel git cvs cvsps2 perl-libwww perl-term-readkey perl-mime-tools \
+sudo pacman -Syu
+sudo pacman -S --needed base-devel git cvs cvsps2 perl-libwww perl-term-readkey perl-mime-tools \
 perl-net-smtp-ssl perl-authen-sasl subversion 
 
 wget https://github.com/stuartpb/aur.sh/archive/master.zip
@@ -28,14 +28,14 @@ sudo sed -i '/\[extra\]/i \
 \[haskell\-core\] \
 Server\ \=\ http\:\/\/xsounds\.org\/\~haskell\/core\/\$arch\n' /etc/pacman.conf
 
-sudo dirmngr --daemon > /dev/null 2>&1
+sudo dirmngr > /dev/null 2>&1 &
 sudo pacman-key --init
 sudo pacman-key --populate
 sudo pacman-key -r 4209170B
 sudo pacman-key --lsign-key 4209170B
 
-sudo pacman -S --needed --noconfirm ghc
-sudo pacman -S --needed --noconfirm haskell-regex-base haskell-parsec haskell-syb haskell-mtl haskell-json haskell-temporary
+sudo pacman -S --needed ghc
+sudo pacman -S --needed haskell-regex-base haskell-parsec haskell-syb haskell-mtl haskell-json haskell-temporary
 
 cd `mktemp -d`
 bash <(curl /tmp/aur.sh-master/aur.sh) -si haskell-regex-pcre-builtin haskell-http-conduit
